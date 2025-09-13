478227d9559ab056c897ca641a2744821aca8244
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-09-13 19:55:56 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    console.log(petsData)
+}
+
+petsArea()
